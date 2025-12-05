27da21d0fd8567d62a56a70167951a716541a090
Infan-Jioun <infanjiounrahman20606@gmail.com>
2025-12-05 21:50:15 +0600

    feat file initailze users, bookings, vehicle, auth

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,17 +5,56 @@ export const pool = new Pool({
 
 })
 const connectDB = async () => {
-    // users
-    await pool.query(
-        ``
-    )
-    // vehicle
-    await pool.query(
-        ``
-    )
-    // bookings
-    await pool.query(
-        ``
-    )
+    try {
+        // users
+        await pool.query(
+            `
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL,
+        password TEXT NOT NULL,
+        phone TEXT NOT NULL,
+        role TEXT NOT NULL CHECK (role IN ('admin', 'customer')),
+        created_at TIMESTAMP DEFAULT NOW()
+         
+        )
+        `
+        )
+        // vehicle
+        await pool.query(
+            `
+            CREATE TABLE IF NOT EXISTS vehicles (
+            id SERIAL PRIMARY KEY,
+            vehicle_name TEXT NOT NULL,
+            type TEXT NOT NULL CHECK (type IN ('car', 'bike', 'van', 'SUV')),
+            registration_number TEXT NOT NULL UNIQUE,
+            daily_rent_price NUMERIC(10,2) NOT NULL CHECK (daily_rent_price > 0),
+            availability_status TEXT NOT NULL CHECK (availability_status IN ('available', 'booked')),
+            created_at TIMESTAMP DEFAULT NOW()
+            )
+            `
+        )
+
+        // bookings
+        await pool.query(
+            `
+            CREATE TABLE IF NOT EXISTS bookings (
+            id SERIAL PRIMARY KEY,
+            customer_id INTEGER NOT NULL REFERENCES users(id) on DELETE RESTRICT,
+            vehicle_id INTEGER NOT NULL REFERENCES vehicles(id) on DELETE RESTRICT,
+            rent_start_date DATE NOT NULL,
+            rent_end_date DATE NOT NULL,
+            total_price NUMERIC(12,2) NOT NULL CHECK (total_price >= 0),
+            status TEXT NOT NULL CHECK (status IN ('active', 'cancelled', 'returned')) DEFAULT 'active',
+            created_at TIMESTAMP DEFAULT NOW(),
+            CHECK (rent_end_date > rent_start_date)
+            ) 
+            `
+        )
+    }
+    catch (err: any) {
+        console.error("connection err", err)
+    }
 }
 export default connectDB; 
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,10 @@ import app from "./app";
 import config from "./config";
 import connectDB from "./config/db";
 connectDB();
-app.use("")
+// app.use("/api/v1/auth/signup");
+// app.use("/api/v1/auth/signin");
+// app.use("/api/v1/auth/users");
+// app.use("/api/v1/bookings");
 
 app.listen(config.port, () => {
     console.log(`Vehicle data server running port : ${config.port} `);
