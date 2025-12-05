6c36a31edddcd77f0f984ada7584b16056b9d2df
Infan-Jioun <infanjiounrahman20606@gmail.com>
2025-12-05 19:07:35 +0600

    add PostgreSQL

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 const app = express();
-
+app.use(express.json())
 app.get("/", (req: Request, res: Response) => {
-    res.send("Vehicle  data server running ")
+    res.send("Vehicle  data server running now")
 })
 export default app;
